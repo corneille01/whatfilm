f738e3499df4bc9c91ead77945d6231bbd5bc057
corneille01 <blogoos1@gmail.com>
2026-08-13 18:15:15 +0200

    village

--- frontend/sw.js
+++ frontend/sw.js
@@ -2,9 +2,9 @@ const CACHE_NAME = "pelify-app-v7";
 
 const APP_SHELL = [
   "/",
-  "/frontend/style.css?v=91",
-  "/frontend/script.js?v=91",
-  "/frontend/filming.js?v=66",
+  "/frontend/style.css?v=92",
+  "/frontend/script.js?v=92",
+  "/frontend/filming.js?v=68",
   "/frontend/manifest.webmanifest",
   "/.well-known/assetlinks.json",
 
